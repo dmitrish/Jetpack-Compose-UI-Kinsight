47588277768c2f3713cf68529cf5a06967908e50
dmitrish <dmitrinyc@yahoo.com>
2020-01-05 15:51:33 -0500

    Initial commit

---
+++ app/src/main/java/com/ds/jetpackcomposekinsight/AppMain.kt
@@ -0,0 +1,41 @@
+package com.ds.jetpackcomposekinsight
+
+
+import androidx.compose.Composable
+import androidx.compose.state
+import androidx.compose.unaryPlus
+import androidx.ui.animation.Crossfade
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.surface.Surface
+import com.ds.jetpackcomposekinsight.screens.WelcomeScreen
+
+
+@Composable
+fun AppMain() {
+    var currentScreen by +state<Screen> { Screen.Welcome }
+    //var viewModel by +state <IdeasViewModel> {IdeasViewModel()}
+
+    // currentScreen = Screen.Ideas()
+
+    fun changeScreen(){
+        currentScreen = Screen.Ideas()
+    }
+
+    MaterialTheme() {
+        Crossfade(currentScreen) { screen ->
+            // Surface() {
+            when (screen) {
+                is Screen.Welcome -> WelcomeScreen(changeScreen = { changeScreen() })
+                is Screen.Ideas -> ideasScreen()
+            }
+            // }
+        }
+    }
+}
+
+
+
+public sealed class Screen {
+    object Welcome : Screen()
+    class Ideas() : Screen()
+}
---
+++ app/src/main/java/com/ds/jetpackcomposekinsight/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.ds.jetpackcomposekinsight
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.*
+import androidx.lifecycle.LiveData
+import androidx.ui.core.*
+import androidx.ui.foundation.DrawImage
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.vector.DrawVector
+import androidx.ui.layout.*
+import androidx.ui.material.*
+import androidx.ui.res.imageResource
+import androidx.ui.res.vectorResource
+import androidx.ui.text.TextStyle
+import androidx.ui.tooling.preview.Preview
+//import com.kinsight.kinsightmultiplatform.models.IdeaModel
+
+
+import androidx.lifecycle.Observer
+import androidx.ui.core.Text
+import androidx.ui.core.dp
+import androidx.ui.core.setContent
+import androidx.ui.layout.Column
+import androidx.ui.layout.Padding
+import androidx.ui.material.MaterialTheme
+import com.ds.jetpackcomposekinsight.viewmodels.IdeasViewModel
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: IdeasViewModel by lazy {
+        IdeasViewModel()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppMain()
+            // mainLayout(ideasViewModel = viewModel)
+        }
+    }
+
+
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
+
+
